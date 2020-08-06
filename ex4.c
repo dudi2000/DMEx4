e317763b0b4b21463cda566abea297c2c3c47a92
dudi2000 <69299438+dudi2000@users.noreply.github.com>
2020-08-06 18:02:05 +0900

    ユーザー名追加

--- ex4.c
+++ ex4.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int main(void) {
+    srand((unsigned int)time(NULL));
+ 
+ char str[6];
+
+ printf("Who are you?\n");
+    scanf("%s", str);
+    printf("Hello, %s!\n", str);
+
  for(int i = 1; i <= 3; i++) {
         printf("Round %d: ", i);
 
